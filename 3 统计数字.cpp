f15887f7e13da9ff465a12a082b9e5d29c4d9361
zlldt <zlldtis@gmail.com>
2017-10-01 14:47:12 +0800

    Create 3 统计数字.cpp
    计算数字k在0到n中的出现的次数，k可能是0~9的一个值

---
+++ 3 统计数字.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /*
+     * param k : As description.
+     * param n : As description.
+     * return: How many k's between 0 and n.
+     */
+    int digitCounts(int k, int n) {
+        // write your code here
+        int count = 0;
+        int number = 0;
+        for ( ; number < n+1 ; number++ ){
+            if ( number == k)
+            count++;
+            if (number>=10){
+            if ( (number/10) == k)
+            count++;
+            }
+            if(number>=100){
+            if ( (number/100) == k)
+            count++;
+            }
+            if(number>=1000){
+            if ( (number/1000) == k)
+            count++;
+            }
+            if(number>=10000){
+            if ( (number/10000) == k)
+            count++;
+            }
+        }
+        return count;
+    }
+};
