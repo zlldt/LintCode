9cc8bfb22ad954cc036e71128d1457b0611507ce
zlldt <zlldtis@gmail.com>
2017-10-01 14:45:10 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param: : An integer
+    @param: : An integer
+    @return: The sum of a and b
+    """
+
+    def aplusb(self, a, b):
+        return -(-a-b)
+        # write your code here
+        #给出两个整数a和b, 求他们的和, 但不能使用 + 等数学运算符。
