85b225fdf40505f60277afde246962b1e0a8538f
zlldt <zlldtis@gmail.com>
2017-10-06 16:42:48 +0800

    Create 9fizz-buzz.cpp

---
+++ 9fizz-buzz.cpp
@@ -0,0 +1,23 @@
+#include <string>
+class Solution {
+public:
+    /*
+     * @param n: An integer
+     * @return: A list of strings.
+     */
+    vector<string> fizzBuzz(int n) {
+        vector<string> v(n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                v[i-1]="fizz buzz";
+            } else if (i % 5 == 0) {
+                v[i-1]="buzz";
+            } else if (i % 3 == 0) {
+                v[i-1]="fizz";
+            } else {
+                v[i-1]=to_string(i);
+            }
+        }
+        return v;
+    }
+};
