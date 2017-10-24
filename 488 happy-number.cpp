6d09664c16dbb9bc31e44dffb09ef1bc239766d2
zlldt <zlldtis@gmail.com>
2017-10-24 22:36:59 +0800

    Create 488 happy-number.cpp

---
+++ 488 happy-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    /*
+     * @param n: An integer
+     * @return: true if this is a happy number or false
+     */
+    int calc(int n){
+        int lower=0,sum=0;
+        for(;n>9;){
+            lower=n%10;
+            n/=10;
+            sum+=lower*lower;
+        }
+        sum+=n*n;
+        return sum;
+    }
+    bool isHappy(int n) {
+        // write your code here
+        if(n==3||n==4||n==5)
+        return false;
+        while(n>2){
+            if(n==3||n==4||n==5)
+        	return 0;
+            n=calc(n);
+        }
+        if(n!=1)
+        return false;
+        if(n==1)
+        return true;
+    }
+};
