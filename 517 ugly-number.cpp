18cf55923b20829e05986c6fb99e75f570bd5c41
zlldt <zlldtis@gmail.com>
2017-11-16 22:17:40 +0800

    Update 517 ugly-number.cpp

--- 517 ugly-number.cpp
+++ 517 ugly-number.cpp
@@ -8,15 +8,7 @@ public:
         // write your code here
         if(num<=0)
             return false;
-        if(num==1)
-            return true;
-        if(num==2)
-            return true;        
-        if(num==3)
-            return true;
-        if(num==4)
-            return false;
-        if(num==5)
+        if(num<5)
             return true;
             
         while(num%5==0 || num%3==0 || num%2==0){
