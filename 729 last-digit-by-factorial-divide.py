2ccc8b0e595bb39970899fd230056f2d5993b7b9
zlldt <zlldtis@gmail.com>
2018-03-04 14:20:40 +0800

    Create 729 last-digit-by-factorial-divide.py
    729. 阶乘除法的最后一位数
    给出两个数 A 和 B, 其中 B >= A. 我们需要计算结果 F 的最后一位数是什么, 其中F = B! / A!(1 <= A, B <= 10^18, A 和 B 非常大)

---
+++ 729 last-digit-by-factorial-divide.py
@@ -0,0 +1,17 @@
+class Solution:
+    """
+    @param A: the given number
+    @param B: another number
+    @return: the last digit of B! / A! 
+    """
+
+    def computeLastDigit(self, A, B):
+        # write your code here
+        factor=1
+        value = B
+        while value> A:
+            factor=(value*factor)%10
+            if int(factor)==0:
+                return int(factor)
+            value-=1
+        return int(factor)
