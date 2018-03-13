01e458f896c0cf4b01a6062a14d6ec71072ba583
zlldt <zlldtis@gmail.com>
2018-03-13 21:52:30 +0800

    Create 74 first-bad-version.py

---
+++ 74 first-bad-version.py
@@ -0,0 +1,17 @@
+class Solution:
+    """
+    @param: n: An integer
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        # write your code here
+        start, end = 1, n
+        while start + 1 < end:
+            mid = (start + end)//2
+            if SVNRepo.isBadVersion(mid):
+                end = mid
+            else:
+                start = mid
+        if SVNRepo.isBadVersion(start):
+            return start
+        return end
