1dcda8c9763acfb00a542335345356bc0e9d5fe5
zlldt <zlldtis@gmail.com>
2018-03-25 22:09:10 +0800

    Create 845 greatest-common-divisor.py

---
+++ 845 greatest-common-divisor.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param a: the given number
+    @param b: another number
+    @return: the greatest common divisor of two numbers
+    """
+    def gcd(self, a, b):
+        # write your code here
+        if a%b ==0:
+            return b
+        return self.gcd(b, a%b)
