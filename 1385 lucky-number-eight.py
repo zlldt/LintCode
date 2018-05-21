d31de9aa46d4165057d418da40814c80bde9038d
zlldt <zlldtis@gmail.com>
2018-05-21 17:09:36 +0800

    Update 1385 lucky-number-eight.py

--- 1385 lucky-number-eight.py
+++ 1385 lucky-number-eight.py
@@ -8,29 +8,35 @@ class Solution:
         result = 0
         position = 0
         matrix = 1
+        sum1 = 0
+        tailnumber = 0
         if n>9:
-            temp = n%10
-            if temp>=8:
+            temp = n % 10
+            if temp >= 8:
                 result += matrix
-            sum = matrix
-            position +=1
+            sum1 = matrix
+            tailnumber += temp
+            position += 1
             n = n//10
             matrix *= 10
-        while n>9:
-            temp = n%10
-            if temp<8:
-                result += sum*temp
-            if temp==8:
-                result += sum*8+matrix
-            if temp==9:
-                result += sum*9+matrix
-            sum = sum*9 + matrix
-            position +=1
+        while n > 9:
+            temp = n % 10
+            if temp < 8:
+                result += sum1*temp
+            if temp == 8:
+                result += sum1*8+matrix
+            if temp == 9:
+                result += sum1*9+matrix
+            sum1 = sum1*9 + matrix
+            tailnumber += temp*matrix
+            position += 1
             n = n//10
             matrix *= 10
-        temp = n%10
-        if temp<8:
-            result += sum*temp
-        else:
-            result += sum*8 + matrix
+        temp = n % 10
+        if temp < 8:
+            result += sum1*temp
+        elif temp == 8:
+            result = sum1*8 + tailnumber + 1
+        elif temp == 9:
+            result += sum1*8 + matrix
         return result
