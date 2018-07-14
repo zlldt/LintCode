c7ef5c699482a56dadafca30625917b2199c0a14
zlldt <zlldtis@gmail.com>
2018-07-14 22:02:06 +0800

    Update 1209 construct-the-rectangle.py

--- 1209 construct-the-rectangle.py
+++ 1209 construct-the-rectangle.py
@@ -3,18 +3,36 @@ class Solution:
     @param area: web pageâs area
     @return: the length L and the width W of the web page you designed in sequence
     """
+
     def constructRectangle(self, area):
         # Write your code here
-        results=[]
-        W=1
-        L=area//W
-        results.append((L,W,L-W))
-        while L>=W :
-            W+=1
-            if area%W==0:
-                L=area//W
-                if L>=W:
-                    results.append([L,W,L-W])
-        results=sorted(results, key=lambda x: x[2])
+        # results=[]
+        # W=1
+        # L=area//W
+        # results.append((L,W,L-W))
+        # while L>=W :
+        #     W+=1
+        #     if area%W==0:
+        #         L=area//W
+        #         if L>=W:
+        #             results.append([L,W,L-W])
         # print(results)
-        return results[0][0],results[0][1]
+        # results=sorted(results, key=lambda x: x[2])
+        # print(results)
+        # return results[0][0], results[0][1]
+
+        temp = 1
+        W = 1
+        L = area // W
+        while L >= W:
+            if area % W == 0:
+                L = area // W
+                if L >= W:
+                    templ = L
+                    tempw = W
+            W += 1
+        return templ, tempw
+
+
+test = Solution()
+test.constructRectangle(50)
